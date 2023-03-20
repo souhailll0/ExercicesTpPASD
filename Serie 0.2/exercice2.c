510442d6332e8ab1cb0b95a3887b5e7158a97cde
Houda Mouttalib <houda.mouttalib@gmail.com>
2023-03-20 17:21:19 +0000

    Q1 Q2

--- Serie 0.2/exercice2.c
+++ Serie 0.2/exercice2.c
@@ -6,6 +6,31 @@ typedef struct noeud{
     struct noeud *suivant;
 }noeud;
 
+struct noeud* inserer(struct noeud* tete, int v){
+    struct noeud* n = malloc(sizeof(struct noeud));
+    n->valeur = v;
+    n->suivant = tete;
+
+    return n;
+}
+
+
+struct noeud* insererQueue(struct noeud* tete, int v){
+    struct noeud* n = malloc(sizeof(struct noeud));
+    n->valeur = v;
+    n->suivant = NULL;
+    if(tete == NULL){
+        tete = n;
+    }else{
+        struct noeud* temp = tete;
+        while(temp->suivant!= NULL){
+            temp = temp->suivant;
+        }
+        temp->suivant = n;
+    }
+    return tete;
+}
+void affiche
 void main(){
     
 }
