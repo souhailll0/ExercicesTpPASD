71f42dd569b88d6808f51065983a34af02d92974
Houda Mouttalib <houda.mouttalib@gmail.com>
2023-03-20 15:29:28 +0000

    correction exercice 2 Q 1 2 3 4

---
+++ Serie 0.2/exercice2.c
@@ -0,0 +1,53 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct noeud{
+    int valeur;
+    struct noeud* suivant;
+};
+
+struct noeud* inserer(struct noeud* tete, int v){
+    struct noeud* n = malloc(sizeof(struct noeud));
+    n->valeur = v;
+    n->suivant = tete;
+
+    return n;
+}
+
+
+void insererQueue(struct noeud* tete, int v){
+    struct noeud* n = malloc(sizeof(struct noeud));
+    n->valeur = v;
+    n->suivant = NULL;
+    if(tete == NULL){
+        tete = n;
+    }else{
+        while(tete->suivant!= NULL){
+            tete = tete->suivant;
+        }
+        tete->suivant = n;
+    }
+}
+void afficher(struct noeud* t){
+    while(t!=NULL){
+        printf("%d\n", t->valeur);
+        t = t->suivant;
+    }
+}
+void afficherRecursive(struct noeud* tete){
+    if(tete != NULL){
+        printf("%d\n", tete->valeur);
+        afficherRecursive(tete->suivant);
+    }
+}
+
+void main(){
+    struct noeud* tete = NULL;
+    tete = inserer(tete, 5);
+    tete = inserer(tete, 6);
+    insererQueue(tete, 7);
+    insererQueue(tete, 8);
+    afficherRecursive(tete);
+
+}
