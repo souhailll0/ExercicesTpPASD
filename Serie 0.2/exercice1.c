b724b1a400545d420dcd8d6ce82e2332c671e51f
Houda Mouttalib <houda.mouttalib@gmail.com>
2023-03-22 12:08:53 +0000

    correction exercice1

--- Serie 0.2/exercice1.c
+++ Serie 0.2/exercice1.c
@@ -6,58 +6,39 @@ typedef struct noeud{
     struct noeud *suivant;
 }noeud;
 
-int recherche(noeud* tete, int v){
-    noeud *p= tete;
-    while(p!= NULL){
-        if(p->valeur == v) return 1;
-        p = p->suivant;
-    }
-    return 0;
-}
-
 noeud* ajouter_debut(noeud* tete){
-    noeud* p = malloc(sizeof(noeud));
-    printf("Donner la valeur: ");
-    scanf("%d", &p->valeur);
-    p->suivant = tete;
-    return p;
-}
-
-void ajouter_fin(noeud* tete){
     noeud* n = malloc(sizeof(noeud));
     printf("Donner la valeur: ");
     scanf("%d", &n->valeur);
-    noeud* p = tete;
-    while(p->suivant != NULL){
-        p = p->suivant;
+    n->suivant = tete;
+    return n;
+}
+noeud* ajouter_fin(noeud* tete){
+    noeud* n = malloc(sizeof(noeud));
+    printf("Donner la valeur: ");
+    scanf("%d", &n->valeur);
+    n->suivant = NULL;
+    if(tete == NULL){
+        tete = n;
+    }else{
+        noeud* aide = tete;
+        while(aide->suivant != NULL){
+            aide = aide->suivant;
+        }
+        aide->suivant = n;
     }
-    p->suivant = n;
+    return tete;
 }
-noeud* supprimer_debut(noeud* tete){
-    noeud* aide = tete->suivant;
-    free(tete);
-    return aide;
-}
-
-void afficher(noeud * tete){
-    noeud *aide = tete;
-    while(aide!= NULL){
-        printf("%d\n", aide->valeur);
-        aide = aide->suivant;
+void afficher(noeud*tete){
+    while(tete!= NULL){
+        printf("%d\n", tete->valeur);
+        tete = tete->suivant;
     }
 }
-
 int main(){
     noeud* tete = NULL;
-    int i;
-    // teste d'ajout de 5 noeuds 
-    for( i = 0; i < 5; i++){
-        tete = ajouter_debut(tete);
-        afficher(tete);
-    }
-
-    tete = supprimer_debut(tete);
+    tete = ajouter_debut(tete);
+    tete = ajouter_debut(tete);
     afficher(tete);
-    
     return 0;
 }
