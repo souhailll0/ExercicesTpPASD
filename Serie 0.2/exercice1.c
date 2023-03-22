88d9589fbb8288c80eb2b7fef63fceb25c5d1f0a
Houda Mouttalib <houda.mouttalib@gmail.com>
2023-03-22 15:25:52 +0000

    correction exercice1

--- Serie 0.2/exercice1.c
+++ Serie 0.2/exercice1.c
@@ -13,6 +13,16 @@ noeud* ajouter_debut(noeud* tete){
     n->suivant = tete;
     return n;
 }
+noeud* supprimer(noeud* tete){
+    if(tete == NULL){
+        return NULL;
+    }else{
+        noeud* t = tete->suivant;
+        free(tete);
+        return t;
+    }
+}
+
 noeud* ajouter_fin(noeud* tete){
     noeud* n = malloc(sizeof(noeud));
     printf("Donner la valeur: ");
@@ -29,16 +39,23 @@ noeud* ajouter_fin(noeud* tete){
     }
     return tete;
 }
+
 void afficher(noeud*tete){
     while(tete!= NULL){
         printf("%d\n", tete->valeur);
         tete = tete->suivant;
     }
 }
+
 int main(){
     noeud* tete = NULL;
-    tete = ajouter_debut(tete);
-    tete = ajouter_debut(tete);
+    tete = ajouter_fin(tete);
+    tete = ajouter_fin(tete);
+    tete = ajouter_fin(tete);
+    tete = ajouter_fin(tete);
+    afficher(tete);
+    printf("apres suppression: \n");
+    tete = supprimer(tete);
     afficher(tete);
     return 0;
 }
